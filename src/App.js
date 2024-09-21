557af5e53d8ec8d6f2828defed85d1f572a85753
Kiran Kumar <kirankumar@Kiran-Kumars-MacBook-Air.local>
2024-09-21 09:29:06 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Axios from 'axios';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   //Setting up the initial state using the react hook "useState"
   return(
     <div>
       <Header/>
+      <div className="main-container">
       <Nav/>
+      <Main/>
+      </div>
     </div>
   )
 }
